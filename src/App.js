2327dbbc365254b29373be6e404a0d6b1d2a3ab3
mayu y <mayublade@gmail.com>
2021-09-04 12:31:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React  from 'react';
+
+class App extends React.Component
+{ 
+  constructor()
+  {
+    super()
+    this.state={counter:0}
+    this.increment=this.increment.bind(this)
+  }
+
+increment()
+{
+  
+  this.setState(prevState=>{
+    return  {counter: prevState.counter+1}
+  })
+  console.log(this.state.counter)
+}
+componentDidUpdate()
+{
+  console.log(this.state.counter)
+}
+decrement=()=>
+{
+  if(this.state.counter >0)
+  {
+  this.setState(prevState=>{
+    return { counter: prevState.counter-1}
+  })
+   }
+}
+
+reset=()=>{
+  this.setState({counter:0})
+}
+
+  render()
+  {
+    return(
+      <div className="container">
+        <div className="innerContainer">
+          <h2>Counter</h2>
+           <input type="text" id="counterValue" disabled value={this.state.counter}></input> 
+           <div className="buttonClass">
+             <div><button onClick={this.increment}>+</button></div>
+             <div><button  onClick={this.decrement}>-</button></div>
+           </div>
+           <div className="reset" onClick={this.reset}><button>reset</button></div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
